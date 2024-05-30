7aec5be9c69450fb48135b0c067215a16679178c
Ilia <isinetov@mail.ru>
2024-05-30 13:49:05 +0400

    Починил VoiceMessageConsroller. Проблема была в отсутствии подключения IFileHandler, AudioFileHandler к DI контейнеру services.

--- SFTelegramBot/Controllers/VoiceMessageController.cs
+++ SFTelegramBot/Controllers/VoiceMessageController.cs
@@ -1,22 +1,32 @@
-﻿using Telegram.Bot;
+﻿using SFTelegramBot.Configuration;
+using SFTelegramBot.Services;
+using Telegram.Bot;
 using Telegram.Bot.Types;
 
 namespace SFTelegramBot.Controllers
 {
-    internal class VoiceMessageController
+    public class VoiceMessageController
     {
+        private readonly AppSettings _appSettings;
         private readonly ITelegramBotClient _telegramClient;
+        private readonly IFileHandler _audioFileHandler;
 
-        public VoiceMessageController(ITelegramBotClient telegramBotClient)
+        public VoiceMessageController(AppSettings appSettings, ITelegramBotClient telegramBotClient, IFileHandler audioFileHandler)
         {
+            _appSettings = appSettings;
             _telegramClient = telegramBotClient;
+            _audioFileHandler = audioFileHandler;
         }
 
         public async Task Handle(Message message, CancellationToken ct)
         {
-            Console.WriteLine($"Контроллер {GetType().Name} получил сообщение");
+            var fileId = message.Voice?.FileId;
+            if (fileId == null)
+                return;
 
-            await _telegramClient.SendTextMessageAsync(message.Chat.Id, $"Получено голосовое сообщение", cancellationToken: ct);
+            await _audioFileHandler.Download(fileId, ct);
+
+            await _telegramClient.SendTextMessageAsync(message.Chat.Id, "Голосовое сообзщение загружено", cancellationToken: ct);
         }
     }
 }
--- SFTelegramBot/Program.cs
+++ SFTelegramBot/Program.cs
@@ -13,6 +13,9 @@ namespace SFTelegramBot
         static void ConfigureServices(IServiceCollection services) 
         {
             AppSettings appSettings = BuildAppSettings();
+            services.AddSingleton(appSettings);
+
+            services.AddSingleton<IFileHandler, AudioFileHandler>();
 
             services.AddTransient<DefaultMessageController>();
             services.AddTransient<VoiceMessageController>();
@@ -29,7 +32,10 @@ namespace SFTelegramBot
         {
             return new AppSettings()
             {
-                BotToken = "7455469646:AAGgh25oMYcY7U4lZ5AhY9M2ocP5Q2v_9ZA"
+                DownloadsFolder = "C:\\Users\\Dinkles\\Downloads",
+                BotToken = "7455469646:AAGgh25oMYcY7U4lZ5AhY9M2ocP5Q2v_9ZA",
+                AudioFileName = "audio",
+                InputAudioFormat = "ogg",
             };
         }
 
